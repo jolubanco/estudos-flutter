4e66e22ccf9aa1ddf7f492aadc5e30ae892bc422
João Thome <joao.thome@renovretail.com.br>
2021-07-09 16:52:41 -0300

    :tada: Iniciando primeiro curso de Flutter

---
+++ bytebank/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    Column(
+      children: <Widget>[
+        Text(
+          'Deliver features faster',
+          textDirection: TextDirection.ltr,
+        ),
+        Text(
+          'Craft beautiful UIs',
+          textDirection: TextDirection.ltr,
+        ),
+        Text(
+          'Craft beautiful UIs',
+          textDirection: TextDirection.ltr,
+        ),
+        Column(
+          children: <Widget>[
+            Text(
+              'Craft beautiful UIs',
+              textDirection: TextDirection.ltr,
+            ),
+          ],
+        )
+      ],
+    ),
+  );
+}
