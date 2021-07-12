5493fe3cfb424ecdb4af4ec7ea5ac4fbc53f8991
João Thome <joao.thome@renovretail.com.br>
2021-07-12 15:32:04 -0300

    criando os primeiros cards

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -2,29 +2,56 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Column(
-      children: <Widget>[
-        Text(
-          'Deliver features faster',
-          textDirection: TextDirection.ltr,
+    MaterialApp(
+      home: Scaffold(
+        body: ListaTransferencias(),
+        appBar: AppBar(title:Text('Transferências')),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {  },
+          child: Icon(Icons.add),
         ),
-        Text(
-          'Craft beautiful UIs',
-          textDirection: TextDirection.ltr,
         ),
-        Text(
-          'Craft beautiful UIs',
-          textDirection: TextDirection.ltr,
-        ),
-        Column(
-          children: <Widget>[
-            Text(
-              'Craft beautiful UIs',
-              textDirection: TextDirection.ltr,
-            ),
-          ],
-        )
-      ],
-    ),
+      ),
   );
 }
+  class ListaTransferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+          children: [
+           Itemtransferencia(Transferencia(100.0,1000)),
+           Itemtransferencia(Transferencia(200.0,1000)), 
+          ],
+    );
+  }
+  }
+
+  class Itemtransferencia extends StatelessWidget {
+
+    final Transferencia _transferencia;
+
+  Itemtransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+              child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text(_transferencia.valor.toString()),
+                subtitle: Text(_transferencia.numeroConta.toString()),
+              ),
+            );
+  }
+    
+  }
+
+  class Transferencia {
+    final double valor;
+    final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+
+  }
+    
+
+
